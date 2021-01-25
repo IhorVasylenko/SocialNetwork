7806486b2161b0c2d91365e5f534114de8eb205a
Ihor Vasylenko <theone116355@gmail.com>
2021-01-25 23:49:15 +0200

    25.01 add map

---
+++ new-app/src/component/User/content/wall/messages/dialogs/Dialogs.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import s from './Dilogs.module.css';
+import {NavLink} from "react-router-dom";
+
+const DialogItem = (props) => {
+    let path = '/messages/' + props.id;
+
+    return <li className={s.dialog}>
+        <NavLink to={path}>
+            <div className={s.contact}>
+                <img src={process.env.PUBLIC_URL + props.avatar} alt=""/>
+                <div className={s.preview}>
+                    <p>{props.name}</p>
+                    <small>{props.passed}</small>
+                    <div className={s.seen}>
+                        {props.seen}
+                    </div>
+                </div>
+            </div>
+        </NavLink>
+    </li>
+}
+
+export default DialogItem
---
+++ new-app/src/component/User/content/wall/messages/letters/Letters.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import s from './Letters.module.css';
+
+
+
+const ContactMessages = (props) => {
+
+    return <div className={s.contactMessages}>
+        <img src={process.env.PUBLIC_URL + props.avatar} alt=""/>
+        <div className={s.message}>
+            <div className={s.header}>
+                <div className={s.name}>
+                    {props.name}
+                </div>
+                <div className={s.passed}>
+                    {props.passed}
+                </div>
+            </div>
+            <div className={s.text}>
+                {props.message}
+            </div>
+        </div>
+    </div>
+}
+
+const MyMessages = (props) => {
+
+    return <div className={s.myMessages}>
+        <img src={process.env.PUBLIC_URL + props.avatar} alt=""/>
+        <div className={s.message}>
+            <div className={s.header}>
+                <div className={s.name}>
+                    {props.name}
+                </div>
+                <div className={s.passed}>
+                    {props.passed}
+                </div>
+            </div>
+            <div className={s.text}>
+                {props.message}
+            </div>
+        </div>
+    </div>
+}
+
+export {
+    ContactMessages,
+    MyMessages
+};
