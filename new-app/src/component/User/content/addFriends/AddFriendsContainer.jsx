8b7409dfc5dfe1f40a3e0a54b9a97109402cd8b8
Ihor Vasylenko <theone116355@gmail.com>
2021-02-04 23:17:52 +0200

    04.02

---
+++ new-app/src/component/User/content/addFriends/AddFriendsContainer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import s from './AddFriends.module.css';
+
+const UserContentAddFriends = (props) => {
+    return (
+        <div className={s.wrapper}>body</div>
+    )
+}
+
+export default UserContentAddFriends
